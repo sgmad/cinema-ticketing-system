9b9e98448dbdfdab7b9f83b32045db8a3a3d89d0
sgmad <155355629+sgmad@users.noreply.github.com>
2025-12-10 09:17:11 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import tkinter as tk
 from gui.customer_home import CustomerHome
 
 def main():
+    root = tk.Tk()
+    root.withdraw() # Hides the small blank window
+    
+    # Launch the Application Window
     app = CustomerHome()
-    app.run()
+    
+    # Ensure closing the app kills the root
+    # Without this, the process keeps running in the background
+    app.protocol("WM_DELETE_WINDOW", lambda: root.destroy())
+    
+    # Start
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
